b8c7a05b1112b0a9e35729f46cabe5ae0f4cb287
Tiago <tiago.faia@catenamedia.com>
2018-05-23 15:46:39 +0100

    meta title change

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'My Nuxt Blog',
+    title: 'iTunes Search Engine',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
